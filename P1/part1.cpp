654987a571306966631b43253c1098669411ea85
ARichards08 <88149323+ARichards08@users.noreply.github.com>
2023-12-02 13:44:46 +0000

    First part setup of 2023 AoC

---
+++ P1/part1.cpp
@@ -0,0 +1,46 @@
+// Included packages
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <cmath>
+
+// Classes
+
+// Functions
+
+// Program
+int main() {
+
+
+/////////////
+// File input
+
+
+std::string line;
+std::vector<std::string> readings;
+
+// Open input file
+std::ifstream inputfile;
+inputfile.open("input.txt", std::ios::in);
+
+// Read file into array
+while(true){
+    std::getline(inputfile, line);
+    if(!inputfile) break; // test eof to break
+
+    readings.push_back(line);
+};
+
+// Close input file
+inputfile.close();
+
+//////////////
+// Calculation
+
+
+
+
+}
